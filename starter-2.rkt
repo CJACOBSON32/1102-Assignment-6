98e1c3fdd0335a75e8678084d5e62d430848e501
CJACOBSON32 <34342644+CJACOBSON32@users.noreply.github.com>
2018-10-03 13:17:07 -0400

    Added Starter and gitignore

---
+++ starter-2.rkt
@@ -0,0 +1,49 @@
+#lang racket
+
+;; #:transparent makes structures display a bit nicer
+(define-struct graph (name nodes) #:transparent #:mutable)
+
+(define-struct node (name edges) #:transparent #:mutable)
+
+;; *******************************************************
+;; you don't need to understand this part
+;; just be sure you use my-eval instead of eval
+(define-namespace-anchor a)
+(define ns (namespace-anchor->namespace a))
+
+
+(define-syntax my-eval
+  (syntax-rules ()
+    [(my-eval arg)
+     (eval arg ns)]))
+;; *******************************************************
+
+;; some examples of Racket weirdness you will need to work around
+(define x 3)
+(define y 5)
+(set! y 6)
+
+;; in the interpreter, try:
+;;     (set! x 10)
+;;     (set! y 10)
+;; first one fails, second one works
+;; --> if you want a variable you can change later, will need both
+;;     define and set! in your macro
+;; (create z 99) could be a handy macro to define and set a variable
+
+(define z (list y))
+;          (list 6)
+(set! y 8)
+;; in the interpreter, try
+;;        y
+;;        z
+;; it looks like values don't update properly.  how can we make it update properly?
+
+(define z2 (list (quote y)))
+(set! y 11)
+;; what is z2?
+;; it's '(y)
+;; how to get back the 11?
+
+;; how about (my-eval (first z2)) ?
+;;    -> that's how we'll store lists of nodes
